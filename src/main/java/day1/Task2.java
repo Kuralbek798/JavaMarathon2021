8e40d1caf6e78b0f92752f23c982dfea56b11d27
Kuralbek798 <agubaev_kuralbek@mail.ru>
2022-03-15 01:30:26 +0600

    14032022

--- src/main/java/day1/Task2.java
+++ src/main/java/day1/Task2.java
@@ -2,6 +2,14 @@ package day1;
 
 public class Task2 {
     public static void main(String[] args) {
+        /*1. Вывести на экран слово “JAVA”, в строку, чтобы оно повторилось 10 раз, используя цикл while.
+        Вывод в консоль должен быть таким:
+        JAVA JAVA JAVA JAVA JAVA JAVA JAVA JAVA JAVA JAVA
 
+        2. Повторить задание 1, но используя цикл for.*/
+
+        for (int i = 0; i < 10; i++) {
+            System.out.print("JAVA" + " ");
+        }
     }
 }
