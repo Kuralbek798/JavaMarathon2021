7c8df70014957527bd9069647ce1d70b4bc7467a
Kuralbek798 <agubaev_kuralbek@mail.ru>
2022-03-17 00:44:11 +0600

    17032022

--- src/main/java/day2/Task3.java
+++ src/main/java/day2/Task3.java
@@ -1,7 +1,40 @@
 package day2;
 
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
+       /* 3. Реализовать программу №2, используя цикл while.
+        2. Есть два числа, которые задаются пользователем через консоль (назовем эти два числа a и b).
+       Используя цикл for, вывести все числа из диапазона между a и b, которые делятся на 5 без остатка,
+       но при этом не делятся на 10 без остатка. Например, число 15 подходит под наше условие
+        (делится на 5 без остатка и не делится на 10 без остатка), но число 20 не подходит под наше условие
+        (делится на 5 без остатка и делится на 10 без остатка). Сами числа a и b в диапазоне не учитывать.
+        Если a >= b вывести сообщение "Некоррект sый ввод". */
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
 
+        numbersRange(a,b);
     }
+
+    public  static  int numbersRange(int a, int b){
+
+        while ( a+1 !=b) {
+            a++;
+            if (a % 5 == 0 && a % 10 != 0) {
+                System.out.print(a +"\t"+"|  ");
+            }
+
+
+            if  (a % 10 == 0){
+                System.out.println(a);
+            }
+        }
+
+        return a;
+    }
+
+
 }
+
--- src/main/java/day2/Task4.java
+++ src/main/java/day2/Task4.java
@@ -1,7 +1,38 @@
 package day2;
 
+import java.util.Scanner;
+
 public class Task4 {
     public static void main(String[] args) {
 
+       /*
+       if x >=5         y = (x^2 - 10) / (x + 7)
+       if -3 < x < 5    y = (x+3) / (x^2 - 2)
+       else             y = 420
+
+        Реализовать программу, которая принимает на вход через консоль с помощью класса Scanner, число x.
+       Для этого числа, по формуле выше, необходимо вычислить значение y и вывести его в консоль,
+        используя метод System.out.println().
+        (Для этих вещественных чисел x и y необходимо использовать тип double и метод nextDouble() у Scanner’а соответственно,
+        чтобы считать из консоли число x). */
+
+        Scanner  scanner = new Scanner(System.in);
+        double x = scanner.nextDouble();
+        double y = 0;
+        System.out.println( mathEquations(x,y));
+
     }
+    public  static  double mathEquations(double x,double y){
+        if (x >= 5){
+            y = (Math.pow(x,2) - 10) / (x + 7);
+        }
+        else if (-3 < x && x < 5){
+            y = (x + 3) / (Math.pow(x,2) - 2);
+        }
+        else {
+            y = 420;
+        }
+    return y;
+    }
+
 }
