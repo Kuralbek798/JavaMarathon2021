4ebf6a0d7fc20f8c21ffe5301dd84fcbe656027b
Kuralbek798 <agubaev_kuralbek@mail.ru>
2022-03-19 00:31:11 +0600

    18032022

--- src/main/java/day3/Task3.java
+++ src/main/java/day3/Task3.java
@@ -1,7 +1,54 @@
 package day3;
+/*
+Реализовать программу, которая 5 раз запрашивает от пользователя два числа - делимое и делитель.
+Для этих двух чисел программа рассчитывает результат деления и выводит его в консоль.
+Если пользователь вводит 0 в качестве делителя, вместо того, чтобы останавливать работу цикла принудительно,
+ мы пропускаем итерацию и выводим в консоль сообщение “Деление на 0”.
+  Ключевое слово else или два if использовать в этой программе нельзя. В решении обязательно использовать оператор continue.
+*/
+
+import java.util.Scanner;
 
 public class Task3 {
+ static    double i;
     public static void main(String[] args) {
 
+        calculator();
+
     }
+
+        public static void calculator(){
+            Scanner scanner = new Scanner(System.in);
+
+
+            while ( i != 5) {
+                double divisible = scanner.nextDouble();
+                double divider = scanner.nextDouble();
+                i++;
+               if(divider==0 || divisible == 0){
+                   System.out.println("Деление на ноль");
+                   continue;
+               }
+
+                double  s= divisible/divider;
+                System.out.println(s);
+            }
+
+
+
+
+
+
+
+
+
+
+
+         }
+
+
+
+
+
+
 }
