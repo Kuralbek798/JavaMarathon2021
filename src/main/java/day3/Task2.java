032ba737148d4d61d116d78d07afd395162f9bc6
Kuralbek798 <agubaev_kuralbek@mail.ru>
2022-03-18 23:08:24 +0600

    18032022

--- src/main/java/day3/Task2.java
+++ src/main/java/day3/Task2.java
@@ -1,7 +1,29 @@
 package day3;
 
+/*
+2. Реализовать программу, которая пока работает, принимает на вход от пользователя два числа - делимое и делитель.
+Для этих двух чисел программа рассчитывает результат деления и выводит его в консоль. Программа останавливает свою работу тогда,
+когда пользователь вводит 0 в качестве делителя.
+ (для этих вещественных чисел необходимо использовать тип double и метод nextDouble() у Scanner’а соответственно).
+В решении обязательно использовать оператор break.
+*/
+
+import java.util.Scanner;
+
 public class Task2 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+         double divisible = scanner.nextDouble();
+         double divider = scanner.nextDouble();
+         calculator(divisible,divider);
+    }
 
+    public static void calculator(double divisible, double divider) {
+     if (divisible == 0 || divider == 0){
+         System.out.println("На ноль делить нельзя");
+         return;
+
+     }
+        System.out.println(divisible/divider);
     }
 }
