25d72a7a448ef83644c0b0e2510ad2410fcadcd8
Kuralbek798 <agubaev_kuralbek@mail.ru>
2022-04-03 01:02:16 +0600

    03042022

---
+++ src/main/java/day5/Car.java
@@ -0,0 +1,31 @@
+package day5;
+
+public class Car {
+   private String model;                                                     //Создать класс Автомобиль (англ. Car) с полями
+   private String  color;                                                    // “Модель”, “Цвет”, “Год выпуска”.
+   private int productYear;
+
+   public String getModel() {
+      return model;
+   }
+
+   public void setModel(String model) {
+      this.model = model;
+   }
+
+   public String getColor() {
+      return color;
+   }
+
+   public void setColor(String color) {
+      this.color = color;
+   }
+
+   public int getProductYear() {
+      return productYear;
+   }
+
+   public void setProductYear(int productYear) {
+      this.productYear = productYear;
+   }
+}
