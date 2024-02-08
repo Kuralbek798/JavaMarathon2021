d069839575afab1af0fbdf6527212e17e1b825d7
куралбек агубаев <agubaev_kuralbek@mail.ru>
2024-02-09 04:55:24 +0600

    3,1

--- src/main/java/day3/Task2.java
+++ src/main/java/day3/Task2.java
@@ -1,5 +1,4 @@
 package day3;
-
 /*
 2. Реализовать программу, которая пока работает, принимает на вход от пользователя два числа - делимое и делитель.
 Для этих двух чисел программа рассчитывает результат деления и выводит его в консоль. Программа останавливает свою работу тогда,
@@ -7,9 +6,25 @@ package day3;
  (для этих вещественных чисел необходимо использовать тип double и метод nextDouble() у Scanner’а соответственно).
 В решении обязательно использовать оператор break.
 */
-
 import java.util.Scanner;
-
 public class Task2 {
-
+public static void main(String[]args){
+    double divisible =  0;
+    double divider = 0;
+    double result = 0;
+    boolean run = true;
+    Scanner scanner = new Scanner(System.in);
+    while(run){
+        System.out.print("Введите делимое: ");
+        divisible = scanner.nextDouble();
+        System.out.print("Введите делитель: ");
+        divider = scanner.nextDouble();
+        if(divider == 0){
+            System.out.print("Вы ввели ноль программа остановлена");
+            break;
+        }
+        result = divisible/divider;
+        System.out.printf("Результат деления %.2f\n",result);
+    }
+  }
 }
--- src/main/java/day3/Task3.java
+++ src/main/java/day3/Task3.java
@@ -7,8 +7,34 @@ package day3;
   Ключевое слово else или два if использовать в этой программе нельзя. В решении обязательно использовать оператор continue.
 */
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+
 public class Task3 {
- //static    double i;
+    public static void main(String[] args) {
+
+        List<Double> doubleList = new ArrayList<>();
+        int i = 0;
+        Scanner scanner = new Scanner(System.in);
+        double divisible = 0;
+        double divider = 0;
+        double result = 0;
+        while (i < 2) {
+            System.out.printf("Введите делимое: ");
+            doubleList.add(scanner.nextDouble());
+            System.out.printf("Введите делитель: ");
+            doubleList.add(scanner.nextDouble());
+            i++;
+        }
+        for (int j = 0; j < doubleList.size() - 1; j += 2) {
+            for (int k = j + 1; k != j + 2; k++) {
+                divisible = doubleList.get(j);
+                divider = doubleList.get(k);
+                result = divisible / divider;
+            }
+            System.out.printf("результат деления: %.2f\n", result);
+        }
+    }
 
 }
